5bf31de9f23a5036d92be22ed86daa601e89ba2d
Christian Iradukunda B <91727952+Christianib003@users.noreply.github.com>
2022-10-18 11:45:32 +0200

    correct error

--- queries.sql
+++ queries.sql
@@ -7,5 +7,4 @@ SELECT date_of_birth from animals WHERE name = 'Agumon' or name ='Pikachu';
 SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name NOT LIKE 'Gabumon';
-SELECT * from animals WHERE weight_kg;
 SELECT * from animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
